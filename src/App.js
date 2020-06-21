4039227be155053f02ef4b3e9d40deb6ab3759cf
AnoojBalaraman <anooj.balaraman@meetrics.com>
2020-06-21 11:38:31 +0200

    updating Input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Input from './components/UI/Input/Input'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    registrationForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Name",
+        },
+        value: ""
+      },
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your E-mail",
+        },
+        value: ""
+      },
+      gender: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            { value: "male", displayValue: "Male" },
+            { value: "female", displayValue: "Female" },
+          ],
+        },
+        value: ""
+      },
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "Password",
+        },
+        value: "",
+      }
+    },
+    isFormValid: false,
+  };
+
+  registrationHandler = (event) => {
+
+  };
+  inputChangeHandler = (event, inputIdentifier) => {
+    let updateRegistrationForm = {...this.state.registrationForm};
+    let updateRegistrationElement = updateRegistrationForm[inputIdentifier]
+    updateRegistrationElement["value"] = event.target.value;
+    updateRegistrationForm[inputIdentifier] = updateRegistrationElement;
+    this.setState({registrationForm: updateRegistrationForm})
+  };
+  render() {
+    const formElementsArray = [];
+
+      for(let key in this.state.registrationForm) {
+        formElementsArray.push({
+          id: key,
+          config: this.state.registrationForm[key]
+        });
+      }
+    return (
+      
+      <div>
+        <h4>Enter Registration Details</h4>
+        <form onSubmit={this.registrationHandler}>
+        {formElementsArray.map(formElement => (
+          <Input 
+            key={formElement.id}
+            elementType={formElement.config.elementType} 
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            changed={(event) => this.inputChangeHandler(event, formElement.id)}
+            />))}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
